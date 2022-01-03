083e311b3a043cbd1f4a074eab3ef199efd63d5a
Oliver <olivercai@uchicago.edu>
2022-01-03 13:16:41 -0500

    updated associations

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :posts do
+    resources :comments
+  end
+
+  root 'posts#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_03_132808) do
+ActiveRecord::Schema.define(version: 2022_01_03_181234) do
 
   create_table "comments", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "content"
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -23,6 +25,8 @@ ActiveRecord::Schema.define(version: 2022_01_03_132808) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "title"
     t.text "content"
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,4 +41,6 @@ ActiveRecord::Schema.define(version: 2022_01_03_132808) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "users"
+  add_foreign_key "posts", "users"
 end
